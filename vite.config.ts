16ad83412997e17dc83264dc766a948d4c1ab4ed
marcos <marc0sdurval20@gmail.com>
2022-09-03 09:01:29 -0300

    chore: lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    include: ['__tests__/**/*.spec.{ts,tsx}'],
-    environment: 'jsdom',
-    coverage: {
-      branches: 80,
-      functions: 80,
-      lines: 80
-    }
-  },
-  server: {
-    port: 3000
-  }
+	plugins: [react()],
+	test: {
+		include: ['__tests__/**/*.spec.{ts,tsx}'],
+		environment: 'jsdom',
+		coverage: {
+			branches: 80,
+			functions: 80,
+			lines: 80
+		}
+	},
+	server: {
+		port: 3000
+	}
 
-})
+});

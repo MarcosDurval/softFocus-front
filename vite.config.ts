b56220fcd903fedd5e83eeb88afbce30ac6a9378
marcos <marc0sdurval20@gmail.com>
2022-09-03 21:59:53 -0300

    test: config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	test: {
-		include: ['__tests__/**/*.spec.{ts,tsx}'],
-		environment: 'jsdom',
+		include: ["__tests__/**/*.spec.{ts,tsx}"],
+		environment: "jsdom",
+		globals: true,
 		coverage: {
 			branches: 80,
 			functions: 80,

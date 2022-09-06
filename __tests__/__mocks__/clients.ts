490f4f125db20118c21a99b78bb9cb3ecd87562a
marcos <marc0sdurval20@gmail.com>
2022-09-06 13:08:01 -0300

    test: mock clients

---
+++ __tests__/__mocks__/clients.ts
@@ -0,0 +1,46 @@
+export const clients = [
+	{
+		"id": 1,
+		"event": "Chuva Excessiva",
+		"name": "person",
+		"email": "user@user.com",
+		"cpf": "11815684984",
+		"type_tillage": "feijão",
+		"date": "2022-09-09",
+		"latitude": "-8.124245",
+		"longitude": "-160.301121"
+	},
+	{
+		"id": 2,
+		"event": "Chuva Excessiva",
+		"name": "person",
+		"email": "user@user.com",
+		"cpf": "11815684984",
+		"type_tillage": "milho",
+		"date": "2022-09-09",
+		"latitude": "-8.124245",
+		"longitude": "-179.301121"
+	},
+	{
+		"id": 3,
+		"event": "Chuva Excessiva",
+		"name": "person",
+		"email": "user@user.com",
+		"cpf": "11815684984",
+		"type_tillage": "feijão",
+		"date": "2022-09-09",
+		"latitude": "-8.124245",
+		"longitude": "-159.301121"
+	},
+	{
+		"id": 4,
+		"event": "Chuva Excessiva",
+		"name": "person",
+		"email": "user@user.com",
+		"cpf": "29614430249",
+		"type_tillage": "soja",
+		"date": "2022-09-09",
+		"latitude": "-8.124245",
+		"longitude": "-129.301121"
+	}
+];

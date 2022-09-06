be63413452b0b8440bba1a2fc2898b7ed5a7a1fd
marcos <marc0sdurval20@gmail.com>
2022-09-06 15:01:26 -0300

    feat: create Client interface, Change and InputClient

---
+++ src/interfaces/main.d.ts
@@ -0,0 +1,27 @@
+interface Client {
+  id: string
+  name: string
+  email: string
+  cpf: string
+  type_tillage: string
+  event: string
+  date: Date
+  longitude: number
+  latitude: number
+}
+
+interface Change extends InputClient {
+  handleChange: (e:React.HTMLElement) => void
+}
+
+interface InputClient {
+  name?: string
+  email?: string
+  cpf?: string
+  type_tillage?: string
+  event?: number
+  date?: string
+  longitude?: number
+  latitude?: number
+  confirme?: boolean
+}

9d58e6e1971f2ce7522f05403ffc9d1372241f7f
marcos <marc0sdurval20@gmail.com>
2022-09-07 09:21:58 -0300

    style: apply css to center content

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 
+import "../styles/components/loading.scss";
+
 const NotFound = () => {
 	return (
-		<div>
+		<div id="center">
 			<h1>Parece que você está perdido</h1>
 			<Link to="clients">Inicio</Link>
 		</div>
